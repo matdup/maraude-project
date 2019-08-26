2cd420b52f0af230359fb2c583f7d1dba30fa0af
mat dup <mathiasdupre@gmail.com>
2019-08-26 13:21:48 +0200

    setup routes et controller to verification

--- app/controllers/maraudes_controller.rb
+++ app/controllers/maraudes_controller.rb
@@ -1,5 +1,5 @@
 class MaraudesController < ApplicationController
-before_action :set_maraude, only: [:show, :edit, :update, :destroy]
+before_action :set_maraude, only: [:show]
 
   def index
     @maraudess = Maraude.all
@@ -18,38 +18,7 @@ before_action :set_maraude, only: [:show, :edit, :update, :destroy]
     end
   end
 
-  def new
-    @maraude = Maraude.new
-  end
-
-  def create
-    @maraude = Maraude.new(maraude_params)
-    @maraude.user = current_user
-    if @maraude.save
-    redirect_to maraude_path
-    else
-    render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    @maraude.update(maraude_params)
-    redirect_to maraude_path
-  end
-
-  def destroy
-    @maraude.destroy
-    redirect_to maraudes_path
-  end
-
   private
-
-  def maraude_params
-    params.require(:maraude).permit(:title, :description, :address, :capacity)
-  end
 
   def set_maraude
     @maraude = Maraude.find(params[:id])
--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-def dashboard
-    @maraudes = current_user.maraudes
-    @bookings = current_user.bookings
-    @my_bookings = current_user.bookings_as_owner
-  end
-
-end
