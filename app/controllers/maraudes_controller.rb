a9807c0c56340b8faf97f034a711fbd8709c7e41
mat dup <mathiasdupre@gmail.com>
2019-08-27 17:35:58 +0200

    done

--- app/controllers/maraudes_controller.rb
+++ app/controllers/maraudes_controller.rb
@@ -26,13 +26,11 @@ class MaraudesController < ApplicationController
     @booking = Booking.new
     @maraudes = Maraude.geocoded #returns flats with coordinates
 
-    @markers = @maraudes.map do |maraude|
-      {
-        lat: maraude.ltd_starts,
-        lng: maraude.lng_starts,
-        infoWindow: render_to_string(partial: "info_window", locals: { maraude: maraude })
+    @markers = {
+        lat: @maraude.ltd_starts,
+        lng: @maraude.lng_starts,
+        infoWindow: render_to_string(partial: "info_window", locals: { maraude: @maraude })
       }
-    end
   end
 
 end
