3f27ba8363fbd110f50f8d8dd5a2e84768939709
Wadi BEN MIMOUN <bmnwa@outlook.com>
2019-08-29 15:38:36 +0200

    displaying iti in index and show pages

--- app/controllers/maraudes_controller.rb
+++ app/controllers/maraudes_controller.rb
@@ -1,13 +1,13 @@
 class MaraudesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  
+
   def index
     if params[:query].present?
       @maraudes = Maraude.where(address: params[:query])
     else
       @maraudes = Maraude.all
     end
-    
+
     @maraudes = Maraude.geocoded
     @markers = @maraudes.map do |maraude|
       {
@@ -17,16 +17,17 @@ class MaraudesController < ApplicationController
         lng_ends: maraude.lng_ends,
         infoWindow: render_to_string(partial: "info_window", locals: { maraude: maraude }),
         image_starts_url: helpers.asset_url('pin_starts'),
-        image_ends_url: helpers.asset_url('pin_ends')
+        image_ends_url: helpers.asset_url('pin_ends'),
+        steps: JSON.parse(maraude.direction.first)["routes"].first["legs"].first["steps"]
       }
     end
   end
-  
+
   def show
     @maraude = Maraude.find(params[:id])
     @booking = Booking.new
     @maraudes = Maraude.geocoded #returns flats with coordinates
-    
+
     @markers = {
       lat_starts: @maraude.ltd_starts,
       lng_starts: @maraude.lng_starts,
@@ -36,6 +37,8 @@ class MaraudesController < ApplicationController
       image_starts_url: helpers.asset_url('pin_starts'),
       image_ends_url: helpers.asset_url('pin_ends')
     }
+    direction = JSON.parse(@maraude.direction.first)
+    @steps = direction["routes"].first["legs"].first["steps"]
   end
 
   def new
@@ -47,7 +50,7 @@ class MaraudesController < ApplicationController
     @maraude.asso = current_user.assos.first
     @maraude.save
   end
-  
+
   private
 
   def maraude_params
