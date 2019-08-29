c1433b6267f1d863da95154b9d3342a922c91714
Quentin Debray <qtn.dbr@gmail.com>
2019-08-29 15:51:01 +0200

    added a new tab to my maraudes_as_owner

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
+
   has_many :assos
   has_many :maraudes, through: :assos
 end
