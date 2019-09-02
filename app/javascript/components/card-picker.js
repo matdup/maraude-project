529c03dc97f334d028973acbc813a6750e481ff3
Quentin Debray <qtn.dbr@gmail.com>
2019-09-02 10:15:43 +0200

    added a link to show view without js

--- app/javascript/components/card-picker.js
+++ app/javascript/components/card-picker.js
@@ -1,12 +1,12 @@
-const initCardPicker = () => {
-  const picture = document.querySelectorAll(".picture").forEach((picture) => {
-    picture.addEventListener("click", (event)  => {
-      window.location.href = `maraudes/${picture.dataset.id}`;
-    })
-  });
-};
+// const initCardPicker = () => {
+//   const picture = document.querySelectorAll(".picture").forEach((picture) => {
+//     picture.addEventListener("click", (event)  => {
+//       window.location.href = `maraudes/${picture.dataset.id}`;
+//     })
+//   });
+// };
 
 
 
-export { initCardPicker };
+// export { initCardPicker };
 
