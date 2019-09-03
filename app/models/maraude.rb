ac171e8ad81e9fdfb39172195bf67b849e407099
Quentin Debray <qtn.dbr@gmail.com>
2019-09-03 16:05:11 +0200

    added some css to the dashboard

--- app/models/maraude.rb
+++ app/models/maraude.rb
@@ -7,6 +7,10 @@ class Maraude < ApplicationRecord
   after_validation :direction_json, if: :will_save_change_to_address_end? || :will_save_change_to_address_start?
   mount_uploader :photo, PhotoUploader
 
+  def remaining_places
+    self.capacity.to_i - self.bookings.size
+  end
+
   private
 
   def geocode_address_end
@@ -18,5 +22,6 @@ class Maraude < ApplicationRecord
   def direction_json
     self.direction = JSON.parse(HTTP.get("https://api.mapbox.com/directions/v5/mapbox/walking/#{self.lng_starts},#{self.ltd_starts};#{self.lng_ends},#{self.ltd_ends}?steps=true&access_token=#{ENV['MAPBOX_API_KEY']}").body)
   end
+
 
 end
