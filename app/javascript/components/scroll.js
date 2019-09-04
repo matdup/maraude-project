bd60d050e7e16ab2c1e419e0f27f80df5e0bbf40
michael boutelet <michael.boutelet@outlook.fr>
2019-09-04 12:12:16 +0200

    add scroll js and modification homepage

---
+++ app/javascript/components/scroll.js
@@ -0,0 +1,15 @@
+const scrollToElement = () => {
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const target = event.currentTarget.getAttribute('href');
+      const targetElement = document.querySelector(`${target}`);
+      if (targetElement) {
+        targetElement.scrollIntoView({behavior: 'smooth'});
+      }
+    });
+  });
+}
+
+export{ scrollToElement };
