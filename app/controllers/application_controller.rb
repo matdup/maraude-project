b7adc22012e74043190e87ac38e9622a247c9f68
Quentin Debray <qtn.dbr@gmail.com>
2019-09-05 17:13:21 +0200

    added meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  def default_url_options
+  { host: ENV["www.alveole-paris.org"] || "localhost:3000" }
+  end
+
 end
